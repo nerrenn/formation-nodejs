b07d6fd079360204c02a89573798245778a6c0dd
RAFFOUX Nicolas <nicolas1.raffoux@gmail.com>
2023-03-01 09:53:30 +0100

    test Fastify/rest client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-console.log(process.env.NODE_ENV)
-console.log(process.env.TZ)
-console.log(process.env.HOST)
-console.log(process.env.PORT)
+import fastify from "fastify";
+
+const app = fastify()
+
+app.listen({port: process.env.PORT as any, host: process.env.HOST}, ()=> {
+    console.log(`Le serveur http est prêt sur l'address : ${process.env.HOST}:${process.env.PORT}`)
+})
+
+app.get('/', ()=> {
+    return 'Bienvenue sur mon serveur'
+})
+
+app.get('/hello', ()=> {
+    return'Bonjour tout le monde'
+})
